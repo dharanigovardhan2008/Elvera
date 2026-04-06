7d0de9611510ab5fcb7d8dd6aa5b6778a9018bb2
Dharani Govardhan <palerugopi00@gmail.com>
2026-04-07 00:38:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import { Toaster } from 'react-hot-toast';
